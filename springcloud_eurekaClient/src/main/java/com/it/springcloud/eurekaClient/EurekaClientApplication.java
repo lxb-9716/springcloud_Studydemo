19ff7243b6e1a3a2a7983e5aa7360c6e890fc080
lxb <lxb_9716@163.com>
2021-03-16 22:36:00 +0800

    springCloud微服务 Eureka注册中心学习   注册中心服务端高可用环境搭建/注册中心客户端环境搭建

--- springcloud_eurekaClient/src/main/java/com/it/springcloud/eurekaClient/EurekaClientApplication.java
+++ springcloud_eurekaClient/src/main/java/com/it/springcloud/eurekaClient/EurekaClientApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication  //当前类是springBoot的启动类
 @EnableEurekaClient   //表示该服务是eureka的客户端
-@EnableFeignClients   //允许使用feign组件
+@SpringBootApplication  //当前类是springBoot的启动类
 public class EurekaClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
